e0c887a476766409d6a346f14ff5cb473943e197
shenghaodong <shenghaodong18@gmail.com>
2026-05-07 16:08:17 -0400

    start on file detection

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+path = "C://Users//" #Use static for now but config should fill this
+listItems = os.listdir(path)
+dir_list = os.listdir(path)
+
+#Search up if moving items requires OS permission
+
